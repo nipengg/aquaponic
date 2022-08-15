8ce045b19b0d78b31a22c90a38f291538bb62ed4
Hadad Al Akbar <akbarhadadal@gmail.com>
2022-08-15 09:18:01 +0700

    make button tambah data become hide and show up when click

--- resources/views/kolam.blade.php
+++ resources/views/kolam.blade.php
@@ -19,72 +19,72 @@
     </div>
     <!-- /.content-header -->
 
-<!-- Main content -->
-<section class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Kolam</h3>
-                        <button type="button" class="btn btn-success float-right"><i class="fas fa-plus"></i> Tambah Data</button>
-                        <form>
-                            <div class="card-body" style="padding-top: 2%;">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Nama Kolam</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Area</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                                </div>
-                                <div class="form-group">
-                                    <label>Deskripsi</label>
-                                    <textarea class="form-control" rows="3" placeholder="Enter ..."></textarea>
-                            </div>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header" id="formInput">
+
+
+                            <h3 class="card-title">Kolam</h3>
+                            <button type="button" onclick="inputForm()" class="btn btn-success float-right"><i class="fas fa-plus"
+                                    ></i> Tambah
+                                Data</button>
+
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example2" class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Timestamp</th>
+                                        <th>Suhu</th>
+                                        <th>PH</th>
+                                        <th>Kekeruhan</th>
+                                        <th>Oksigen</th>
+                                        <th>Zat Terlarut</th>
+                                        <th>Oksidasi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                        <td>-</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
                     </div>
-                    </form>
                 </div>
-                <!-- /.card-header -->
-                <div class="card-body">
-                    <table id="example2" class="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th>Timestamp</th>
-                                <th>Suhu</th>
-                                <th>PH</th>
-                                <th>Kekeruhan</th>
-                                <th>Oksigen</th>
-                                <th>Zat Terlarut</th>
-                                <th>Oksidasi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-                </div>
-                <!-- /.card-body -->
+                <!-- /.col -->
             </div>
+            <!-- /.row -->
         </div>
-        <!-- /.col -->
-    </div>
-    <!-- /.row -->
-    </div>
-    <!-- /.container-fluid -->
-</section>
+        <!-- /.container-fluid -->
+    </section>
 
-<script type="text/javascript">
-    document.getElementById("TopTitle").innerHTML = "Kolam";
-    document.getElementById("kolam").innerHTML = '<a href="/kolam" class="nav-link active"><i class="nav-icon fas fa-tachometer-alt"></i><p>Kolam</p></a>';
-</script>
+    <script type="text/javascript">
+        document.getElementById("TopTitle").innerHTML = "Kolam";
+        document.getElementById("kolam").innerHTML =
+            '<a href="/kolam" class="nav-link active"><i class="nav-icon fas fa-tachometer-alt"></i><p>Kolam</p></a>';
+        document.getElementById("TopTitle").innerHTML = "Kolam";
+
+        function noInputForm() {
+            document.getElementById("formInput").innerHTML = '<h3 class="card-title">Kolam</h3><button type="button" onclick="inputForm()" class="btn btn-success float-right"><i class="fas fa-plus"></i> Tambah Data</button>';
+        }
+
+        function inputForm() {
+            document.getElementById("formInput").innerHTML =
+                '<h3 class="card-title">Kolam</h3><button type="button" onclick="noInputForm()" class="btn btn-success float-right"><i class="fas fa-plus"></i> Tambah Data</button><form><div class="card-body" style="padding-top: 2%;"><div style="margin-top:20px;" class="form-group"><label for="exampleInputEmail1">Nama Kolam</label><input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email"></div><div class="form-group"><label for="exampleInputEmail1">Area</label><input type="text" class="form-control" id="exampleInputEmail1" placeholder="Area (m)"></div><div class="form-group"><label>Deskripsi</label><textarea class="form-control" rows="3" placeholder="Enter ..."></textarea></div></div></form>';
+        }
+    </script>
 @endsection
