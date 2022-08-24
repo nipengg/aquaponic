48bb487359ee86cb65aab224d548d73a1307b9f2
nipengg <nevillecorneliustj@gmail.com>
2022-08-25 02:46:05 +0700

    redirect register route to login

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,19 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\GrafikController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\PoolController;
-use App\Http\Middleware\Inactive;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {
+    return redirect('/login');
+});
+
+Route::get('/register ', function () {
     return redirect('/login');
 });
 
